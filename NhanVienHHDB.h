da1f16c3cc3d28a6dbe7738b435bcc45f7b74612
minhhuyti31h1 <50070480+minhhuyti31h1@users.noreply.github.com>
2019-05-23 21:52:21 +0700

    Create NhanVienHHDB.h

---
+++ NhanVienHHDB.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "NhanVienHH.h"
+class NhanVienHHDB: public NhanVienHH
+{
+private:
+double base_salary;
+public:
+NhanVienHHDB();
+NhanVienHHDB(string, string, string,double, double, double);
+//setter;
+void setbase_salary(double base);
+
+//getter;
+double getbase_salary()const;
+double getSalary()const;
+//methods
+double getSalary()const
+{
+	return this->tongdoanhthu * this->mucthuong / 100 + this -> base_salary *0.1;
+}
+~NhanVienHHDB();
+};
