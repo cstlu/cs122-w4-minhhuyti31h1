74061221670b0aad5a06c1ee77e830ad3632803f
minhhuyti31h1 <50070480+minhhuyti31h1@users.noreply.github.com>
2019-05-23 21:50:10 +0700

    Create NhanVienHH.h

---
+++ NhanVienHH.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "NhanVien.h"
+class NhanVienHH:public NhanVien
+{
+protected:
+double tongdoanhthu;
+double mucthuong;//10%
+public:
+NhanVienHH();
+NhanVienHH(string, string, string, double,double);
+//setters
+void settongdoanhthu(double tong);
+void setmucthuong(double thuong);
+
+//getters
+double gettongdoanhthu()const;
+double  getmucthuong()const;
+double getSalary()const;
+
+//method
+double getSalary()const
+{	
+	return this->tongdoanhthu * this->mucthuong / 100;
+}
+~NhanVienHH();
+};
