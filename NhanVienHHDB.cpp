1d2d69215d02ae36dc5dadc4552b0ffe44f0d7f4
minhhuyti31h1 <50070480+minhhuyti31h1@users.noreply.github.com>
2019-05-23 21:53:05 +0700

    Create NhanVienHHDB.cpp

---
+++ NhanVienHHDB.cpp
@@ -0,0 +1,30 @@
+#include "NhanVienHHDB.h"
+
+NhanVienHHDB::NhanVienHHDB()
+{}
+NhanVienHHDB::NhanVienHHDB(string, string, string,double, double, double):id(id), fullname(name), taxcode(tax),tong(tong),thuong(thuong),base(base)
+{}
+//setter;
+void setbase_salary(double base)
+{
+this-> base=base;
+}
+//getter;
+double getbase_salary()const
+{
+return this->base;
+}
+double getSalary()const
+{
+return this->Salary;
+}
+
+//methods
+double getSalary()const
+{
+return this->tongdoanhthu * this->mucthuong / 100 + this -> base_salary *0.1;
+}
+
+NhanVienHHDB::~NhanVienHHDB()
+{
+}
